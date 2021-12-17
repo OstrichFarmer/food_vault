28b5b22a98f45a6cfa03c7e38f697d1807e22715
Stephen Joel <joel.stephen56.sj@gmail.com>
2021-12-17 17:02:38 +0100

    Added the category meal screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -6,6 +6,7 @@ class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
+  // ignore: use_key_in_widget_constructors
   const CategoryItem(this.title, this.color);
 
   @override
---
+++ lib/category_meal_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealScreen extends StatelessWidget {
+  const CategoryMealScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
